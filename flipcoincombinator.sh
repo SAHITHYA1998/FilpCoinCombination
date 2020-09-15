1ca22f07ecc001c93eaaa533be1d07b458e4bde8
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-15 15:10:29 +0530

    Added : message1

--- flipcoincombinator.sh
+++ flipcoincombinator.sh
@@ -1,2 +1,13 @@
-echo welcome flipcoincombinator
+
+
+#! /bin/bash 
+echo "Welcome to Flip coin combination"
+
+randomNum=$(( RANDOM % 2  ))
+if [ $randomNum -eq 1 ]
+then
+        echo "HEAD"
+else
+        echo "TAIL"
+fi
 
