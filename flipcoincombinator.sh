bf5fc72dbb600fef3f9a6f09c7dff19bbb955aea
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-15 15:13:07 +0530

    Added : message2

--- flipcoincombinator.sh
+++ flipcoincombinator.sh
@@ -3,11 +3,37 @@
 #! /bin/bash 
 echo "Welcome to Flip coin combination"
 
+declare -A headTail
+
+times=0
+
+while [ $times -lt 100 ]
+do
 randomNum=$(( RANDOM % 2  ))
+
+
 if [ $randomNum -eq 1 ]
 then
-        echo "HEAD"
-else
-        echo "TAIL"
-fi
+        val=${headTail["Head"]}
+        val=$(($val+1))
 
+        headTail["Head"]=$val
+else
+        val=${headTail["Tail"]}
+        val=$(($val+1))
+
+        headTail["Tail"]=$val
+
+fi
+ times=$(($times+1))
+done
+
+headC=${headTail["Head"]}
+tailC=${headTail["Tail"]}
+
+headPer=$((100*$headC/$times))
+
+tailPer=$((100*$tailC/$times))
+
+echo "Head % is : $headPer"
+echo "Tail % is : $tailPer"
